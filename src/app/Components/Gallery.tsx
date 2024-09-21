c9ef7d2e6f4f331bb6cd91f8fd9d8486c68ccc8f
Emmaline Mercer <eewmercer@gmail.com>
2024-09-20 21:18:16 -0400

    Add click functionality between all pics and prevent going out of bounds

--- src/app/Components/Gallery.tsx
+++ src/app/Components/Gallery.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, {FC, useState} from 'react';
-import bigApple from "../../../public/pictures/bigApple.jpeg"
+import Image from 'next/image'
+
 
 interface GalleryProps {
     name: string;
@@ -10,25 +11,39 @@ interface GalleryProps {
 const Gallery: FC<GalleryProps> = (props) => {
     const [galleryCounter, setGalleryCounter] = useState(0)
 
+    function boundaryChecker() {
+        if (galleryCounter > 6) {
+            setGalleryCounter(galleryCounter-1);
+        } else if (galleryCounter < 0) {
+            setGalleryCounter(galleryCounter+1);
+        } 
+    }
+
+    console.log(galleryCounter)
+    boundaryChecker()
+
     return (
         <>
             <div className='heading'>
                 <h1>Welcome to the Gallery!</h1>
-                <h3>Feel free to peruse the hightly sought after art pieces</h3>
+                <h3>Feel free to peruse the highly sought after art pieces</h3>
             </div>
             <div className='gallery-scroll'>
                 <button onClick={() => {
                     setGalleryCounter(galleryCounter-1);
-                    console.log(galleryCounter)
                 }}>Prev</button>
-                {props.images.map((image) => {
-                    return <div>
-                        <img src={String(bigApple)} alt="bigApple pic" />
-                    </div>
+                {props.images.map((image, index) => {
+                    if (galleryCounter === index) {
+                        return <Image
+                        width={500}
+                        height={500}
+                        src={`/../../../public/pictures/${image}`}
+                        alt={image}
+                    /> 
+                    }                                    
                 })}
                 <button onClick={() => {
                     setGalleryCounter(galleryCounter+1);
-                    console.log(galleryCounter)
                 }}>Next</button>
             </div>
         </>
